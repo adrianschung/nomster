e4d3e4cd76608bb1fbc388ad53eb177f92035f9f
Adrian Chung <adrianschung@gmail.com>
2018-02-22 20:20:28 -0500

    Fixes user permissions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -21,14 +21,14 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
     
     if @place.user != current_user
-      return render plain: 'Not Allowed', status :forbidden
+      return render plain: 'Not Allowed', status: :forbidden
     end
   end
   
   def update
     @place = Place.find(params[:id])
     if @place.user != current_user
-      return render plain: 'Not Allowed', status :forbidden
+      return render plain: 'Not Allowed', status: :forbidden
     end
     @place.update_attributes(place_params)
     redirect_to root_path
