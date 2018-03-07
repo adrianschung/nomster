25e594ae5304416780ea033da7bfe3782bd57f69
Adrian Chung <adrianschung@gmail.com>
2018-03-07 15:52:49 -0500

    Adds development environment mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@skatesterapp.com"
   
   def comment_added
-    mail(to: adrianschung@hotmail.com,
-      subject: "A comment has been added to your place"")
+    mail(to: "adrianschung@hotmail.com",
+      subject: "A comment has been added to your place")
   end
 end
