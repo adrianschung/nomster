b293499fc1caeb60c710dbd8572b44fee2f4bb38
Adrian Chung <adrianschung@gmail.com>
2018-03-08 16:53:41 -0500

    Fixes typos and here link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
   
-  config.action_mailer.default_url_options = { host: 'skatester-adrian-chung.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'nomster-adrian-chung.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
